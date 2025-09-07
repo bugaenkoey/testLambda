4ff7f2202f74281337a4494268a9dcf3e9b6306d
Yevhen <buhaienkoyevhen@gmail.com>
2025-09-07 14:57:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2/promise");
+
+exports.handler = async (event) => {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST, // зчитуємо з environment variables
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+
+  const [rows] = await connection.execute("SELECT * FROM users LIMIT 5");
+  await connection.end();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(rows),
+  };
+};
