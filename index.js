b7cb2f9979417d8eeafd1967d5e4509b06fd6c04
Yevhen <buhaienkoyevhen@gmail.com>
2025-09-08 12:50:14 +0300

    add DB_PORT

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2/promise");
 exports.handler = async (event) => {
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST, // зчитуємо з environment variables
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
