b6a4634252f09150a869aaa14d50bca7598127e7
Yevhen <buhaienkoyevhen@gmail.com>
2025-09-17 23:45:35 +0300

    MYSQL_URI

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ exports.handler = async (event) => {
   try {
     console.log("DB config:", process.env.DB_HOST, process.env.DB_USER);
 
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST, // зчитуємо з environment variables
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-    });
+    const connection = await mysql.createConnection(
+      process.env.MYSQL_URI
+      //       {
+      //   host: process.env.DB_HOST, // зчитуємо з environment variables
+      //   port: process.env.DB_PORT,
+      //   user: process.env.DB_USER,
+      //   password: process.env.DB_PASS,
+      //   database: process.env.DB_NAME,
+      //       }
+    );
 
     const [rows] = await connection.execute("SELECT * FROM users LIMIT 5");
     // const [rows] = await connection.execute("SELECT * FROM services LIMIT 5");
